9eadad773c9d0634e805d849de6d17762f9826e8
Yuxin Wang <yuxinwang.dev@gmail.com>
2025-03-11 21:49:42 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,116 @@
+import { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import { IPosition } from "monaco-editor";
+import Grid from "@mui/material/Unstable_Grid2";
+import { loadGoggleWasm } from "./utils/wasm.ts";
+import { ASTViewer } from "./viewers/ASTViewer.tsx";
+import { CFGViewer } from "./viewers/CFGViewer.tsx";
+import { SSAViewer } from "./viewers/SSAViewer.tsx";
+import { SourceEditor } from "./viewers/SourceEditor.tsx";
+import { Title } from "./title/Title.tsx";
+
+// We store the Go source and the last cursor position in local storage such that users will not lose their input.
+const storedSrc = localStorage.getItem("src") ?? "";
+const storedPos: IPosition = {
+  lineNumber: Number(localStorage.getItem("srcLine")) ?? 1,
+  column: Number(localStorage.getItem("srcCol")) ?? 1,
+};
+
+export const App = () => {
+  const [src, setSrc] = useState<string>(storedSrc);
+  const [srcPos, setSrcPos] = useState<IPosition>(storedPos);
+
+  const [ast, setAST] = useState<string>("");
+  const [cfgs, setCFGs] = useState<string>("");
+  const [ssa, setSSA] = useState<string>("");
+
+  // Start Go WebAssembly such that the parse function is available in global this.
+  // Then, load the source and position from local storage and set then.
+  useEffect(() => {
+    loadGoggleWasm().then(() => {
+      handleSrcChange(storedSrc);
+      handleSrcPosChange(storedPos);
+    });
+  }, []);
+
+  // Update source and position in local storage anytime they change.
+  useEffect(() => localStorage.setItem("src", src), [src]);
+  useEffect(() => {
+    localStorage.setItem("srcLine", srcPos.lineNumber.toString());
+    localStorage.setItem("srcCol", srcPos.column.toString());
+  }, [srcPos]);
+
+  const handleSrcChange = (code: string | undefined) => {
+    const renderError = (error: string) => {
+      setAST(`ERROR: ${error}`);
+      setCFGs(`ERROR: ${error}`);
+      setSSA(`ERROR: ${error}`);
+    };
+
+    if (code === undefined) {
+      renderError("no source input");
+      return;
+    }
+
+    // @ts-expect-error: `parse` is injected into global this by the Goggle WebAssembly module.
+    if (globalThis.parse === undefined) {
+      renderError("WASM: Go wasm may not have been initialized yet");
+      return;
+    }
+
+    setSrc(code);
+
+    // @ts-expect-error: `parse` is injected into global this by the Goggle WebAssembly module.
+    const result = globalThis.parse(code);
+    if (result.error !== undefined) {
+      renderError(result.error);
+      return;
+    }
+
+    const body = JSON.parse(result.body);
+    setAST(body.ast);
+    setCFGs(body.cfgs);
+    setSSA(body.ssa);
+  };
+
+  const handleSrcPosChange = (pos: IPosition) => setSrcPos(pos);
+
+  return (
+    <Box>
+      <Grid container>
+        <Grid xs={12} height="64px">
+          <Title />
+        </Grid>
+
+        <Grid container height="calc(100vh - 64px)" width="100%">
+          <Grid
+            xs={6}
+            sx={{
+              borderRight: "1px solid lightgray",
+              borderBottom: "1px solid lightgray",
+            }}
+          >
+            <SourceEditor
+              initialContent={src}
+              initialPosition={srcPos}
+              onChange={handleSrcChange}
+              onCursorChange={(event) => handleSrcPosChange(event.position)}
+            />
+          </Grid>
+          <Grid xs={6} sx={{ borderBottom: "1px solid lightgray" }}>
+            <ASTViewer src={src} srcPos={srcPos} ast={ast} />
+          </Grid>
+
+          <Grid xs={6} sx={{ borderRight: "1px solid lightgray" }}>
+            <CFGViewer src={src} srcPos={srcPos} cfgs={cfgs} />
+          </Grid>
+          <Grid xs={6}>
+            <SSAViewer src={src} srcPos={srcPos} ssa={ssa} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "@fontsource/inter";
+import "@fontsource/jetbrains-mono";
+import "@fontsource/jetbrains-mono/800.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
