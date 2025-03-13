1c9ec355c7969b28042d9a54c01a40a48646da3c
Yuxin Wang <yuxinwang.dev@gmail.com>
2025-03-12 21:48:55 -0400

    Set 4K resolution for screenshot

--- .github/workflows/screenshot.py
+++ .github/workflows/screenshot.py
@@ -3,6 +3,8 @@ from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
     browser = p.chromium.launch()
     page = browser.new_page()
+    # Set 4K resolution.
+    page.set_viewport_size({"width": 3840, "height": 2160})
     page.goto("http://localhost:3000")
     page.wait_for_load_state('load')
     page.screenshot(path="assets/screenshot.png")
