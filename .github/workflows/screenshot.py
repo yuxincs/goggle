27f06aa91c29d996811f18fdb5b99b61c756acf4
Yuxin Wang <yuxinwang.dev@gmail.com>
2025-03-12 22:18:24 -0400

    Correct high-DPI setting

--- .github/workflows/screenshot.py
+++ .github/workflows/screenshot.py
@@ -3,8 +3,8 @@ from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
     browser = p.chromium.launch()
     context = browser.new_context(
-        viewport={'width': 3840, 'height': 2160},  # 4K resolution
-        device_scale_factor=4,  # 4x UI scaling
+        viewport={"width": 1920, "height": 1080},
+        device_scale_factor=2,  # Set scale factor to 2 for high-DPI
     )
     page = context.new_page()
     page.goto("http://localhost:5173")
