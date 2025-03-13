f893132343bf543e5698e0747444de790538c8f8
Yuxin Wang <yuxinwang.dev@gmail.com>
2025-03-12 21:47:48 -0400

    Wait for the page to load before taking a screenshot

--- .github/workflows/screenshot.py
+++ .github/workflows/screenshot.py
@@ -4,5 +4,6 @@ with sync_playwright() as p:
     browser = p.chromium.launch()
     page = browser.new_page()
     page.goto("http://localhost:3000")
+    page.wait_for_load_state('load')
     page.screenshot(path="assets/screenshot.png")
     browser.close()
