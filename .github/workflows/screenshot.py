00514f59f0dc982524ea89e1caf4d3175f65f928
Yuxin Wang <yuxinwang.dev@gmail.com>
2025-03-12 22:09:08 -0400

    Use 4K resolution with 2X UI scaling and wait for network idle event

--- .github/workflows/screenshot.py
+++ .github/workflows/screenshot.py
@@ -2,10 +2,12 @@ from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
     browser = p.chromium.launch()
-    page = browser.new_page()
-    # Set 4K resolution.
-    page.set_viewport_size({"width": 3840, "height": 2160})
+    context = browser.new_context(
+        viewport={'width': 3840, 'height': 2160},  # 4K resolution
+        device_scale_factor=2,  # 2x UI scaling
+    )
+    page = context.new_page()
     page.goto("http://localhost:5173")
-    page.wait_for_load_state('load')
+    page.wait_for_load_state('networkidle')
     page.screenshot(path="dist/assets/screenshot.png")
     browser.close()
